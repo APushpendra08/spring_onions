854801ca908c980f500b92afe9c5dee856709bb6
A Pushpendra <aladupushpendra08@gmail.com>
2023-06-10 12:56:58 +0530

    Project Init
    Project init

---
+++ src/test/java/com/play/reminder/Reminder/ReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play.reminder.Reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
