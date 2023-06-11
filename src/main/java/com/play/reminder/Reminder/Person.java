17a95c2ddd45979d9044a1e998e556e5658c7bda
A Pushpendra <aladupushpendra08@gmail.com>
2023-06-11 10:15:17 +0530

    mongo not working

---
+++ src/main/java/com/play/reminder/Reminder/Person.java
@@ -0,0 +1,26 @@
+package com.play.reminder.Reminder;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+    @Id private String Id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- src/main/java/com/play/reminder/Reminder/StarController.java
+++ src/main/java/com/play/reminder/Reminder/StarController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-@RestController
+//@RestController
 public class StarController {
-
 
     private final AtomicInteger id =new AtomicInteger();
     private final String template = "Hello %s";
